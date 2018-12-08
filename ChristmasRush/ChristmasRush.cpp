74344b1676ba4e7bbb61ea217982e73ab8dfef36
Brian Gariepy <bgariepy@cae.com>
2018-12-08 14:48:12 -0500

    Functional

--- ChristmasRush/ChristmasRush.cpp
+++ ChristmasRush/ChristmasRush.cpp
@@ -12,20 +12,26 @@ using namespace std;
 
 using Position = array<int, 2>;
 
+
 struct AstarPos
 {
     AstarPos()
         : pos({{0,0}})
         , fScore(0)
+        , gScore(0)
     {}
 
-    AstarPos(Position pos, int fscore)
+    AstarPos(Position pos, int fscore, int gscore, vector<string> path)
         : pos(pos)
         , fScore(fscore)
+        , gScore(gscore)
+        , cameFrom(path)
     {}
     
     Position pos;
+    vector<string> cameFrom;
     int fScore;
+    int gScore;
 };
 
 bool operator<(const AstarPos& lhs, const AstarPos& rhs)
@@ -33,7 +39,7 @@ bool operator<(const AstarPos& lhs, const AstarPos& rhs)
     return lhs.fScore > rhs.fScore;
 }
 
-int calcFScore(Position & start, Position & finish)
+int calcHScore(Position & start, Position & finish)
 {
     return abs(start[0] - finish[0]) + abs(start[1] - finish[1]);
 }
@@ -59,43 +65,109 @@ struct Player
 {
     Position position;
     vector<string> quests;
+    map<string, Position> items;
     Tile tile;
     string tileItem;
+
+    void clear()
+    {
+        quests.clear();
+        items.clear();
+    }
 };
 
 struct Board
 {
     array<array<Tile, 7>, 7> grid;
-    map<string, Position> items;
     Player hero, villain;
 
     vector<string> aStar(Player& player, array<int,2> & target)
     {
-        vector<string> answer;
-
         map<Position, Position> cameFrom;
         set<Position> closedSet;
         priority_queue<AstarPos> openSet;
-        openSet.emplace(player.position, calcFScore(player.position, target));
+        openSet.emplace(player.position, calcHScore(player.position, target), 0, vector<string>());
+
+        //cerr << "path" << player.position[0] << player.position[1] << target[0] << target[1] << endl;
 
         while (!openSet.empty())
         {
-            const AstarPos & current = openSet.top;
+            AstarPos current = openSet.top();
+            Position & currentPos = current.pos;
             if (current.pos == target)
             {
-                // return best path
-                return answer;
+                // return best path by going through the cameFroms
+
+                return current.cameFrom;
             }
 
             openSet.pop();
-            closedSet.insert(current.pos);
+            closedSet.insert(currentPos);
 
-            // TODO: determine the neighbors of the position
+            // Determine the neighbors of the position
+            Tile& currentTile = grid[currentPos[0]][currentPos[1]];
+            //cerr << "tile " << currentTile.upPath << currentTile.downPath << currentTile.leftPath << currentTile.rightPath << endl;
 
+            if (currentTile.upPath && currentPos[0] != 0 && grid[currentPos[0] - 1][currentPos[1]].downPath)
+            { 
+                Position newPos = { {currentPos[0] - 1, currentPos[1]} };
+                if (closedSet.find(newPos) == closedSet.end())
+                {
+                    int gScore = current.gScore + 1;
+                    vector<string> path = current.cameFrom;
+                    path.push_back("UP");
+
+                    openSet.emplace(newPos, gScore + calcHScore(newPos, target), gScore, path);
+                }
+            }
+
+            if (currentTile.downPath && currentPos[0] != 6 && grid[currentPos[0] + 1][currentPos[1]].upPath)
+            {
+                Position newPos = { { currentPos[0] + 1, currentPos[1] } };
+                if (closedSet.find(newPos) == closedSet.end())
+                {
+                    int gScore = current.gScore + 1;
+                    vector<string> path = current.cameFrom;
+                    path.push_back("DOWN");
+
+                    openSet.emplace(newPos, gScore + calcHScore(newPos, target), gScore, path);
+                }
+            }
+
+            if (currentTile.leftPath && currentPos[1] != 0 && grid[currentPos[0]][currentPos[1] - 1].rightPath)
+            {
+                Position newPos = { { currentPos[0], currentPos[1] - 1} };
+                if (closedSet.find(newPos) == closedSet.end())
+                {
+                    int gScore = current.gScore + 1;
+                    vector<string> path = current.cameFrom;
+                    path.push_back("LEFT");
+
+                    openSet.emplace(newPos, gScore + calcHScore(newPos, target), gScore, path);
+                }
+            }
+
+            if (currentTile.rightPath && currentPos[1] != 6 && grid[currentPos[0]][currentPos[1] + 1].leftPath)
+            {
+                Position newPos = { { currentPos[0], currentPos[1] + 1 } };
+                if (closedSet.find(newPos) == closedSet.end())
+                {
+                    int gScore = current.gScore + 1;
+                    vector<string> path = current.cameFrom;
+                    path.push_back("RIGHT");
+
+                    openSet.emplace(newPos, gScore + calcHScore(newPos, target), gScore, path);
+                }
+            }
         }
 
+        return vector<string>();
+    }
 
-        return answer;
+    void clear()
+    {
+        hero.clear();
+        villain.clear();
     }
 };
 
@@ -104,9 +176,11 @@ struct Board
 
 int main()
 {
+    Board board;
+    
     // game loop
     while (1) {
-        Board board;
+        board.clear();
         bool moveTurn;
         cin >> moveTurn; cin.ignore();
 
@@ -119,6 +193,8 @@ int main()
                 board.grid[i][j].readType(tile);
             }
         }
+
+        cerr << "check" << endl;
 
         // Read in player information
         for (int i = 0; i < 2; i++) {
@@ -151,12 +227,15 @@ int main()
             {
                 board.villain.tileItem = itemName;
             }
+            else if (itemPlayerId == 0)
+            {
+                board.hero.items[itemName] = { {itemY, itemX} };
+            }
             else
             {
-                board.items[itemName] = { {itemY, itemX} };
+                board.villain.items[itemName] = { { itemY, itemX } };
             }
 
-            // Don't think I need to use itemPlayerID
         }
 
         // Read in quest information
@@ -167,7 +246,7 @@ int main()
             int questPlayerId;
             cin >> questItemName >> questPlayerId; cin.ignore();
 
-            if (questPlayerId = 0)
+            if (questPlayerId == 0)
             {
                 board.hero.quests.push_back(questItemName);
             }
@@ -180,6 +259,8 @@ int main()
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
+        cerr << moveTurn << endl;
+
         if (!moveTurn)
         {
             cout << "PUSH 3 RIGHT" << endl; // PUSH <id> <direction> | MOVE <direction> | PASS
@@ -187,22 +268,32 @@ int main()
         else
         {
             // Start by identifying where I need to go
-            string& goal = board.hero.quests[0];
-            auto res = board.items.find(goal);
+            string& goal = board.hero.quests.front();
+            //cerr << goal << endl;
+            auto res = board.hero.items.find(goal);
 
             // If it isn't on the board, I can't get there, just pass for now
-            if (res != board.items.end())
+            if (res != board.hero.items.end())
             {
+                //cerr << res->first << endl;
                 auto path = board.aStar(board.hero, res->second);
 
-                string move = "MOVE";
-                for (auto & dir : path)
+                if (path.empty())
                 {
-                    move += " ";
-                    move += dir;
+                    cout << "PASS" << endl;
                 }
+                else
+                {
+                    string move = "MOVE";
+                    for (auto & dir : path)
+                    {
+                        move += " ";
 
-                cout << move << endl;
+                        move += dir;
+                    }
+
+                    cout << move << endl;
+                } 
             }
             else
             {
